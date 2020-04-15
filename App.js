0a1cba67d1509228e68d9b5b04bae55d70484b27
bitbro-ui <52121125+bitbro-ui@users.noreply.github.com>
2020-04-15 17:52:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Transactionscreen from './Transactionpage';
+import Searchscreen from './Searchpage';
+
+export default class App extends React.Component{
+  render(){
+  return (
+    <AppContainer/>
+  );
+}
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction:{screen:Transactionscreen},
+  Search:{screen:Searchscreen}
+})
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/Transactionpage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Transactionscreen extends React.Component{
+    render(){
+        return(
+            <View style ={{flex:1,justifyContent:'center',alignItems:"center"}}>
+                <Text>Issue book or Return book</Text>>
+            </View>
+        )
+}
+}
+
+
+
